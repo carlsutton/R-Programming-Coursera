309ee604f8b891694f7d0402b3033a27a2a256cb
Carl Sutton <suttoncarl@ymail.com>
2014-12-28 10:07:21 -0700

    various flailing around at attempts to complete homework

---
+++ rankhospital4.R
@@ -0,0 +1,89 @@
+#  rankhospital <- function(state,outcome,num)
+# {
+        state <- "MD"
+        outcome <- "heart attack"
+        num = "worst"
+        if (num = "best")
+        {
+                num <- 1
+        }  
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        require(data.table)
+        if(outcome !="heart attack" && outcome !="heart failure" && outcome !="pneumonia")
+        {
+                stop("invalid outcome")
+        }
+        if(!(state %in% data[[7]]))
+        {
+                stop("invalid state")
+        }
+        hosp <- data[,2]
+        st <- data[,7]
+        HALME <- as.numeric(data[,13])
+        HFLME <- as.numeric(data[,19])
+        PLME <- as.numeric(data[,25])
+        head(PLME)
+        HA1 <- data.table(hosp,st,HALME)
+        colnames(HA1)
+        ha1 <- subset(HA1, st == state)
+        ha1 <- ha1[complete.cases(ha1),]
+        HF1 <- data.table(hosp,st,HFLME)
+        colnames(HF1)
+        hf1 <- subset(hf1, st == state)
+        hf1 <- as.data.table(hf1[complete.cases(hf1),]) 
+        hf1
+        P1 <- data.table(hosp,st,PLME)
+        head(P1)
+        p1 <- subset(P1, st == state)
+        head(p1)
+        p1 <- p1[complete.cases(p1),]
+        
+        p1
+        #  HALME refers to heart attack low mortality estimate
+        #  setting the keys automatically orders a data table
+        setkey(ha1,HALME,hosp)
+        if (num == "worst")
+        {
+                num <- nrow(ha1)
+                num
+        }
+        ha1       
+        setkey(hf1,HFLME,hosp)
+        if (num == "worst")
+        {
+                num == nrow(hf1)
+        }
+        hf1
+        setnames(p1,c("hosp","st","PLME"))
+        head(p1)        
+        setkey(p1,PLME,hosp)
+        if (num == "worst")
+        {
+                num <- nrow(p1)
+        }
+        p1
+        if (outcome =="heart attack") 
+        {
+                print(ha1[num,hosp])
+                print(class(ha1[num,hosp]))
+                y <- c(as.character(ha1[num,hosp]))
+                return(y)
+        }
+        if (outcome == "heart failure")
+        {
+                head(hf1)
+                print(hf1[num,hosp])
+                print(class(hf1[num,hosp]))
+                y <- c(hf1[num,hosp])
+                return(y)
+        }
+        if (outcome == "pneumonia")
+        {
+                print(p1[num,hosp])
+                print(class(p1[num,hosp]))
+                y <- c( p1[num,hosp])
+                return(y)
+        }
+}               
+y <- rankhospital("MD","heart failure,5")
+is.character(y)
--- test1assng1part2.R
+++ test1assng1part2.R
@@ -1,15 +1,17 @@
 #  testing function code before making the function
 #  Set up arguements to pass to function
 require(data.table)
-id <- 1
+id <- c(2,4,6,8,10,12)
 directory <- "spectdata"
 numFiles <- length(id)
-k <- c(1:length(id))  #initial a vector for the number of files
+rows <- 1:numFiles
 files_temp <- data.frame()      #  initialize a data.frame
 for (i in 1:numFiles) {
         allFiles <- sprintf("%s/%03d.csv","specdata",id) #  file names created
-        files_temp <- read.csv(allFiles[i],header = TRUE) #  read data file 
-        file <- data.table(files_temp)
-        file <- c("id", "nobs")
-        file <- file[, use = "complete.obs"]
+        files_temp <- read.csv(allFiles[i],header = TRUE) #  read data file  
+        files_temp <- files_temp[complete.cases(files_temp),] 
+        file <- data.table(files_temp) 
+        rows[i] <- nrow(file)
+          
 }   
+out <- data.frame("id" =id, "nobs" = rows)
